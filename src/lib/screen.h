5d83bc36941da86e9700216bb64508f5a947f9de
Bernhard Biskup <bernhard@fract-tech.com>
2017-11-26 00:15:27 +0100

    Added click map

--- src/lib/screen.h
+++ src/lib/screen.h
@@ -1,13 +1,29 @@
 #ifndef SCREEN_H
 #define SCREEN_H
 
+#include "widget.h"
+#include <vector>
+#include <memory>
+
+
 namespace bane {
+
+/// Abstraction of terminal
 class Screen {
 public:
   Screen();
   ~Screen();
 
   void waitForKey() const;
+
+private:
+
+  /// Maintain a mapping of screen position to Widget instance
+  /// to delegate mouse clicks
+  /// position on screen (x, y) corresponds to
+  /// y * width + x
+  /// in vector
+  std::unique_ptr<std::vector<Widget*>> clickMap_;
 };
 } // namespace bane
 
