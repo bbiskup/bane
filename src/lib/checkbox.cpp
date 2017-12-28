5842a8ee2996791cf1f26b4b48c6fd0aa40aee93
Bernhard Biskup <bernhard@fract-tech.com>
2017-12-28 10:34:35 +0100

    Represent checkbox using angular brackets

--- src/lib/checkbox.cpp
+++ src/lib/checkbox.cpp
@@ -3,8 +3,8 @@
 #include <boost/log/trivial.hpp>
 
 namespace {
-// 1 for box itself, 1 for space
-constexpr const int checkBoxPartWidth{2};
+// 3 for box itself ([x]), 1 for space
+constexpr const int checkBoxPartWidth{4};
 } // namespace
 
 bane::CheckBox::CheckBox(Widget* root, std::string label, bool isChecked)
@@ -25,7 +25,7 @@ int bane::CheckBox::preferredHeight() const noexcept { return 1; }
 void bane::CheckBox::doRender() {
   BOOST_LOG_TRIVIAL(trace) << "Button::doRender" << y() << " " << x();
   CharPoint orig{origin()};
-  mvaddstr(orig.y, orig.x, isChecked_ ? "x" : "_");
-  mvaddstr(orig.y, orig.x + 1, " ");
-  mvaddstr(orig.y, orig.x + 2, label_.c_str());
+  mvaddstr(orig.y, orig.x, isChecked_ ? "[x]" : "[ ]");
+  mvaddstr(orig.y, orig.x + 3, " ");
+  mvaddstr(orig.y, orig.x + 4, label_.c_str());
 }
