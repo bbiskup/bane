4f7c0ff75697eef704514a2ea652824c4b03f4e3
Bernhard Biskup <bernhard@fract-tech.com>
2018-01-06 18:53:04 +0100

    Updated test case

--- src/main.cpp
+++ src/main.cpp
@@ -44,9 +44,12 @@ int main() {
   // label.resize(10, 20);
   // label.render();
 
-  bane::Label* label1{pane->addChild<bane::Label>("my_label_x")};
-  label1->setText("new_label_text");
+  pane->addChild<bane::Label>("first_label");
+  pane->addChild<bane::Label>("second_label");
   pane->addChild<bane::HLine>();
+  bane::Label* label3 = pane->addChild<bane::Label>("third_label");
+  label3->resize(10, 3);
+
   /*
   std::future<void> job = std::async(std::launch::async, [&label1, &app]() {
     std::this_thread::sleep_for(2s);
@@ -58,36 +61,38 @@ int main() {
   //  pane->addChild<bane::Button>("my_button");
   /* pane->addChild<bane::Button>("my_button_x");*/
 
-  /*bane::Pane* paneH{pane->addChild<bane::Pane>()};
-  paneH->resize(50, 10);*/
-  
-
   /*
+  bane::Pane* paneH{pane->addChild<bane::Pane>()};
+  // paneH->resize(50, 2);
   paneH->setLayoutMgr<bane::HBoxLayoutMgr>();
   paneH->addChild<bane::PushButton>("another_button");
-  paneH->addChild<bane::VLine>();
+  // paneH->addChild<bane::VLine>();
   paneH->addChild<bane::Label>("label_in_paneH");
   BOOST_LOG_TRIVIAL(trace) << "######## " << paneH->height();
+
+  // pane->addChild<bane::PushButton>("my_button");
+  pane->addChild<bane::Label>("my_label");
   */
 
-  pane->addChild<bane::PushButton>("my_button");
-  
-  bane::ButtonGroup* bg1{pane->addChild<bane::ButtonGroup>(
-      "button_group_1", bane::Orientation::vertical)};
-  bg1->addChild<bane::CheckBox>("my_checkbox_1");
-  bg1->addChild<bane::CheckBox>("my_checkbox_2");
-  bg1->addChild<bane::RadioButton>("my_radio_button_1");
-  bg1->addChild<bane::RadioButton>("my_radio_button_2");
-  // bg1->addChild<bane::CheckBox>("my_checkbox_3");
-  
+    bane::ButtonGroup* bg1{pane->addChild<bane::ButtonGroup>(
+        "button_group_1", bane::Orientation::vertical)};
+    bg1->addChild<bane::CheckBox>("my_checkbox_1");
+    bg1->addChild<bane::CheckBox>("my_checkbox_2");
+    bg1->addChild<bane::RadioButton>("my_radio_button_1");
+    bg1->addChild<bane::RadioButton>("my_radio_button_2");
+    // bg1->addChild<bane::CheckBox>("my_checkbox_3");
+     
+  pane->addChild<bane::Label>("fourth_label");
+
+  /*
   pane->addChild<bane::Label>("my_label_x2");
-  //bg1->children()[0].requestFocus();
+  // bg1->children()[0].requestFocus();
   pane->addChild<bane::LineEdit>("My text", 20ul);
   bane::LineEdit* ledx = pane->addChild<bane::LineEdit>("My other text", 30ul,
                                                         "other initial text");
   ledx->requestFocus();
 
-  //pane->addChild<bane::Label>("-------------------------");
+  // pane->addChild<bane::Label>("-------------------------");
   bane::ButtonGroup* bg2{pane->addChild<bane::ButtonGroup>(
       "button_group_2", bane::Orientation::vertical)};
   bg2->addChild<bane::CheckBox>("my_checkbox_3");
@@ -95,7 +100,7 @@ int main() {
 
   bg1->setFocusSuccessor(ledx);
   ledx->setFocusSuccessor(bg2);
-  
+  */
 
   app.run();
 }
